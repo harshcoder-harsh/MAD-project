77d5e9118c11b078c379692198b99bf187e5e48f
harshcoder-harsh <harshraghav701@gmail.com>
2025-11-27 23:13:46 +0530

    Add startup script and fix all TypeScript errors

---
+++ start-expo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Start Expo with Android QR code
+cd /Users/harshvardhan/MAD-project
+echo "Starting Expo Go for Android..."
+echo "Make sure you have Node.js 18+ installed"
+echo ""
+npx expo start --android
+
